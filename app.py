48cac0e7a1cb6f758c70f95c989eea6281ed60c3
darkchoudhary39 <darkchoudhary39@gmail.com>
2025-03-22 14:41:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello from Tech VJ'
 
-
 if __name__ == "__main__":
-    app.run()
-  
+    # Running the app on host '0.0.0.0' makes it accessible externally.
+    app.run(host='0.0.0.0', port=5000, debug=True)
